e9bf9e8ba5e1601deb48338ecf8d9f417ef3fc18
Giang ca <91960480+Giang-st1-00@users.noreply.github.com>
2022-10-24 15:05:44 +0700

    fix to run

--- src/components/layouts/Dashboard.jsx
+++ src/components/layouts/Dashboard.jsx
@@ -1,13 +1,26 @@
 
 import pageRoutes from '../../config/Router';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Layout, Col, Typography } from "antd";
 import Todolist from '../../pages/Todolist';
+const { Header, Content, Sider } = Layout;
 
 const DashBoard = () => {
     // em tính sử dụng antd layout mà install không được .
     // cung tinh dung config mà install react - dom mà không được
-    
     return (
-        <Todolist />
+      <Router>
+          <Content>
+            <Routes>
+                    {pageRoutes.map((route, index) => {
+                      const Page = route.element;
+                      return (
+                        <Route key={index} element={<Page />} path={route.path} />
+                      );
+                    })}
+            </Routes>
+          </Content>
+      </Router>
     )
 }
 
